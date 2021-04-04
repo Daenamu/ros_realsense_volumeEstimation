626b28f0f1b7f259a586e07e4d52cc030032c517
Daenamu <dheodnr318@naver.com>
2021-04-04 15:08:40 +0900

    convert to ros and 3d visualization

--- test/parameters.py
+++
@@ -1,5 +0,0 @@
-WIDTH           =    640
-HEIGHT          =    480
-AVG_FRAME       =    30                # Defines the number of sample frames                                                        
-LOW_CUT_DEPTH   =    1.2               # low cut under 1.2cm error   
-HIGH_CUT_DEPTH  =    10                # high cut over 10cm error
